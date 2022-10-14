e41db4ee9ae37790c665403462da67ca9d05098d
DerMatte <me@dermatte.de>
2022-10-14 17:34:04 +0200

    change hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,22 +15,24 @@ import Contact from "components/Contact";
 import Partner from "components/Partner";
 import Projekte from "components/Projekte";
 
+import banner from "assets/pic/banner_fi-da-team.jpg";
+
 const IndexPage = () => (
   <Layout>
-    <section className="">
-      <img
-        className="h-64 w-screen object-cover md:h-96"
-        src="https://www.fi-da.de/wp-content/uploads/banner_fi-da-team_21.jpg"
-        alt="Hero"
+    <section className="max-w-screen flex flex-col md:container md:mx-auto md:flex-row md:items-center md:px-8">
+      <Image
+        className="h-64 flex-grow-0 object-cover md:h-[50vh] md:w-1/2 md:flex-1 "
+        src={banner}
+        alt="Hero Banner"
       />
       {/* <div className="absolute top-1/2 left-1/2 flex -translate-y-1/2 -translate-x-1/2 flex-col items-center justify-center rounded-sm bg-white/75 p-8 backdrop-blur-lg"> */}
-      <SectionContainer className="">
+      <SectionContainer className="flex-1">
         <div className=" pt-12">
           <h2 className=" text-md uppercase text-gray-700">
             Elektroinstallationen - Photovoltaik & Speicher
           </h2>
-          <h1 className="pt-1 pb-2 text-3xl font-bold">
-            Fischer & Daumüller Elektroinstallationen
+          <h1 className="pt-1 pb-2 text-3xl font-bold md:text-4xl">
+            Fischer & Daumüller <br /> Elektroinstallationen
           </h1>
           <p className="max-w-lg pb-10">
             Das Fi-Da Team ist für Sie rund um den Strom für Sie unterwegs!
@@ -38,7 +40,7 @@ const IndexPage = () => (
         </div>
         <div className="flex gap-4">
           <Link href="#">
-            <a className=" rounded-md border-2 px-4 py-2 decoration-black hover:border-2 hover:border-yellow hover:bg-black/10 ">
+            <a className=" rounded-md border-2 px-4 py-2 decoration-black hover:border-2 hover:border-yellow ">
               Mehr erfahren
             </a>
           </Link>
@@ -53,7 +55,7 @@ const IndexPage = () => (
     <SectionContainer className="py-24">
       <div className="flex flex-col ">
         <h3 className="pb-4 text-2xl font-bold">Unsere Leistungen</h3>
-        <p className="max-w-lg pb-8">
+        <p className="max-w-lg pb-16">
           Die Firma Fi-Da GmbH, Fischer und Daumüller, wurde 1998 von den
           Elektromeistern Hans-Ulrich Fischer und Andreas Daumüller gegründet.
           Unser motiviertes Team von 14 Mitarbeitern ist für den Kunden
@@ -84,7 +86,7 @@ const IndexPage = () => (
     <Partner />
     <Projekte />
     <SectionContainer className="border-y border-black py-24">
-      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4">
         <div className="flex flex-col items-center  gap-4">
           <h4 className="text-5xl font-medium">25</h4>
           <span className="">Jahre Erfahrung</span>
