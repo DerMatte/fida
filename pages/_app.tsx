bf9a6fb8ab62d917329e03b499ee55e94ade7297
DerMatte <me@dermatte.de>
2022-10-18 21:02:07 +0200

    switch sa url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Component {...pageProps} />
-      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <Script src="https://sa.lanas.dev/latest.js" />
       <noscript>
         {/* eslint-disable @next/next/no-img-element */}
         <img
-          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          src="https://sa.lanas.dev/noscript.gif"
           alt=""
           referrerPolicy="no-referrer-when-downgrade"
         />
